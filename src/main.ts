ea4fedda9b95dff2300ac0b0aa89eaa91b48b3ad
Ian-SeoJaeMin <animomin@daum.net>
2024-12-27 16:04:03 +0900

    fix(main): dto의 타입에 맞게 Parameter 형변환 옵션 사용

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,9 @@ async function bootstrap() {
       whitelist: true, // 데코레이터가 없는 속성은 제거
       forbidNonWhitelisted: true,
       // transform: true,
+      transformOptions: {
+        enableImplicitConversion: true, // dto 의 타입에 맞게 자동으로 타입 변경
+      },
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
