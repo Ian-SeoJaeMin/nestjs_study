1a1d3b1ec6a5784913db063a2b0ca88c02032e2f
Ian-SeoJaeMin <animomin@daum.net>
2025-01-02 21:01:18 +0900

    feat(interceptor): Add cacheInterceptor - 인터셉터를 활용해서 메모리 캐싱 연습

---
+++ src/common/interceptor/cache.interceptor.ts
@@ -0,0 +1,31 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable, of, tap } from 'rxjs';
+
+@Injectable()
+export class CacheInterceptor implements NestInterceptor {
+  private cache = new Map<string, any>();
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<any> | Promise<Observable<any>> {
+    const req = context.switchToHttp().getRequest();
+
+    const key = `${req.method}-${req.path}`;
+
+    if (this.cache.has(key)) {
+      return of(this.cache.get(key)); // 일반 변수를 Observable로 반환하기 위해서 of 사용
+    }
+
+    return next.handle().pipe(
+      tap((response) => {
+        this.cache.set(key, response);
+      }),
+    );
+  }
+}
