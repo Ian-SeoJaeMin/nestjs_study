a35217d3da3605208769f730a52e22ac7a3eb36c
Ian-SeoJaeMin <animomin@daum.net>
2025-01-03 22:27:51 +0900

    fix(fileupload): multiple file fields upload using multer - FileFieldsInterceptor

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -22,7 +22,7 @@ import { RBAC } from 'src/auth/decorator/rbac.decorator';
 import { Role } from 'src/user/entities/user.entity';
 import { GetMoviesDto } from './dto/get-movies.dto';
 import { TransactionInterceptor } from 'src/common/interceptor/transaction.interceptor';
-import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { FileFieldsInterceptor, FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('movie')
 @UseInterceptors(ClassSerializerInterceptor) // class-transformer 를 moviceController에 적용
@@ -44,8 +44,27 @@ export class MovieController {
     @Post()
     @RBAC(Role.admin)
     @UseInterceptors(TransactionInterceptor)
-    @UseInterceptors(FilesInterceptor('movies'))
-    postMovie(@Body() movieData: CreateMovieDto, @Request() req: any, @UploadedFiles() files: Express.Multer.File[]) {
+    @UseInterceptors(
+        FileFieldsInterceptor([
+            {
+                name: 'movie',
+                maxCount: 1
+            },
+            {
+                name: 'poster',
+                maxCount: 2
+            }
+        ])
+    )
+    postMovie(
+        @Body() movieData: CreateMovieDto,
+        @Request() req: any,
+        @UploadedFiles()
+        files: {
+            movie?: Express.Multer.File[];
+            poster?: Express.Multer.File[];
+        }
+    ) {
         console.log('-----------------');
         console.log(files);
         return this.movieService.create(movieData, req.queryRunner);
