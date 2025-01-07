ecb16810cddfdc53ecf2433a923f599e7a3af90e
Ian-SeoJaeMin <animomin@daum.net>
2025-01-07 22:00:01 +0900

    fix(fileupload): 선업로드 기능 구현 - common.controller 생성 - post /common/video 엔드포인트 생성 - public/temp 경로에 파일 저장하는 기능 구현

---
+++ src/common/common.controller.ts
@@ -0,0 +1,23 @@
+import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('common')
+export class CommonController {
+    @Post('video')
+    @UseInterceptors(
+        FileInterceptor('video', {
+            limits: {
+                fileSize: 20000000
+            },
+            fileFilter(req, file, callback) {
+                if (file.mimetype !== 'video/mp4') {
+                    return callback(new BadRequestException('MP4 타입의 영상만 업로드 가능합니다.'), false);
+                }
+                return callback(null, true);
+            }
+        })
+    )
+    createVideo(@UploadedFile() video: Express.Multer.File) {
+        return { fileName: video.filename };
+    }
+}
