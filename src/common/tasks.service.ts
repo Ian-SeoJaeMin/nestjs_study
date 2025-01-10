fa09fd42d8622ae535f6fff6cb7f8f102a6f4a97
Ian-SeoJaeMin <animomin@daum.net>
2025-01-10 21:59:31 +0900

    feat(logger): DefaultLogger 추가 및 TasksService에 주입

--- src/common/tasks.service.ts
+++ src/common/tasks.service.ts
@@ -5,12 +5,16 @@ import { readdir, unlink } from 'fs/promises';
 import { join, parse } from 'path';
 import { Movie } from 'src/movie/entity/movie.entity';
 import { Repository } from 'typeorm';
+import { DefaultLogger } from './logger/default.logger';
 
 @Injectable()
 export class TasksService {
-    private readonly logger = new Logger(TasksService.name);
+    // private readonly logger = new Logger(TasksService.name);
 
-    constructor(@InjectRepository(Movie) private readonly movieRepository: Repository<Movie>) {}
+    constructor(
+        @InjectRepository(Movie) private readonly movieRepository: Repository<Movie>,
+        private readonly logger: DefaultLogger
+    ) {}
 
     @Cron('* * * * * *')
     logEverSecond() {
