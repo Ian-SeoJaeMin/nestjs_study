2386e48de7ee6f12eed807b89c57180a5f3bf75d
Ian-SeoJaeMin <animomin@daum.net>
2025-01-13 19:23:09 +0900

    feat(movie, common): Swagger API 문서화를 위한 DTO에 ApiProperty 추가

--- src/movie/dto/create-movie.dto.ts
+++ src/movie/dto/create-movie.dto.ts
@@ -1,26 +1,47 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { ArrayNotEmpty, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateMovieDto {
     @IsNotEmpty()
     @IsString()
+    @ApiProperty({
+        description: '영화의 제목',
+        example: '더 글로리'
+    })
     title: string;
 
     @IsNotEmpty()
     @IsNumber({}, { each: true })
     @Type(() => Number)
     @ArrayNotEmpty()
+    @ApiProperty({
+        description: '장르 아이디',
+        example: [1, 2]
+    })
     genreIds: number[];
 
     @IsNotEmpty()
     @IsString()
+    @ApiProperty({
+        description: '영화의 설명',
+        example: '꾸울잼'
+    })
     detail: string;
 
     @IsNotEmpty()
     @IsNumber()
+    @ApiProperty({
+        description: '영화 감독 아이디',
+        example: 1
+    })
     directorId: number;
 
     @IsNotEmpty()
     @IsString()
+    @ApiProperty({
+        description: '영화 파일 이름',
+        example: 'aaa-bbb-ccc-ddd.mp4'
+    })
     movieFileName: string;
 }
