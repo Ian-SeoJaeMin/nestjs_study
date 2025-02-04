8411aae2d97ae84667923c1a7ad6813d295cb9a1
Ian-SeoJaeMin <animomin@daum.net>
2025-02-04 17:19:58 +0900

    test(genre): add GenreService and GenreController unit tests

--- src/genre/genre.controller.spec.ts
+++ src/genre/genre.controller.spec.ts
@@ -1,20 +1,80 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GenreController } from './genre.controller';
 import { GenreService } from './genre.service';
+import { Genre } from './entities/genre.entity';
+import { CreateGenreDto } from './dto/create-genre.dto';
+import { UpdateGenreDto } from './dto/update-genre.dto';
+
+const mockGenreService = {
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn()
+};
 
 describe('GenreController', () => {
-    let controller: GenreController;
-
+    let genreController: GenreController;
+    let genreService: GenreService;
     beforeEach(async () => {
-        // const module: TestingModule = await Test.createTestingModule({
-        //     controllers: [GenreController],
-        //     providers: [GenreService]
-        // }).compile();
-        // controller = module.get<GenreController>(GenreController);
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [GenreController],
+            providers: [{ provide: GenreService, useValue: mockGenreService }]
+        }).compile();
+        genreController = module.get<GenreController>(GenreController);
+        genreService = module.get<GenreService>(GenreService);
     });
 
     it('should be defined', () => {
-        // expect(controller).toBeDefined();
-        expect(true).toBeDefined();
+        expect(genreController).toBeDefined();
+    });
+
+    describe('findAll', () => {
+        it('should return an array of genres', async () => {
+            const genres = [new Genre(), new Genre()];
+            jest.spyOn(genreService, 'findAll').mockResolvedValue(genres);
+            const result = await genreController.findAll();
+            expect(result).toEqual(genres);
+            expect(genreService.findAll).toHaveBeenCalled();
+        });
+    });
+
+    describe('findOne', () => {
+        it('should return a genre by id', async () => {
+            const genre = new Genre();
+            jest.spyOn(genreService, 'findOne').mockResolvedValue(genre);
+            const result = await genreController.findOne(1);
+            expect(result).toEqual(genre);
+            expect(genreService.findOne).toHaveBeenCalledWith(1);
+        });
+    });
+
+    describe('create', () => {
+        it('should create a new genre', async () => {
+            const createGenreDto = { name: 'Action' };
+            jest.spyOn(genreService, 'create').mockResolvedValue(createGenreDto as Genre);
+            const result = await genreController.create(createGenreDto as CreateGenreDto);
+            expect(result).toEqual(createGenreDto);
+            expect(genreService.create).toHaveBeenCalledWith(createGenreDto);
+        });
+    });
+
+    describe('update', () => {
+        it('should update a genre', async () => {
+            const updateGenreDto = { name: 'Action' };
+            jest.spyOn(genreService, 'update').mockResolvedValue(updateGenreDto as Genre);
+            const result = await genreController.update(1, updateGenreDto as UpdateGenreDto);
+            expect(result).toEqual(updateGenreDto);
+            expect(genreService.update).toHaveBeenCalledWith(1, updateGenreDto);
+        });
+    });
+
+    describe('remove', () => {
+        it('should remove a genre', async () => {
+            jest.spyOn(genreService, 'remove').mockResolvedValue(1);
+            const result = await genreController.remove(1);
+            expect(result).toEqual(1);
+            expect(genreService.remove).toHaveBeenCalledWith(1);
+        });
     });
 });
--- src/genre/genre.service.spec.ts
+++ src/genre/genre.service.spec.ts
@@ -1,18 +1,101 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GenreService } from './genre.service';
+import { Repository } from 'typeorm';
+import { Genre } from './entities/genre.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateGenreDto } from './dto/create-genre.dto';
+import { UpdateGenreDto } from './dto/update-genre.dto';
+import { NotFoundException } from '@nestjs/common';
+
+const mockGenreRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    save: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn()
+};
 
 describe('GenreService', () => {
-    let service: GenreService;
-
+    let genreService: GenreService;
+    let genreRepository: Repository<Genre>;
     beforeEach(async () => {
-        // const module: TestingModule = await Test.createTestingModule({
-        //     providers: [GenreService]
-        // }).compile();
-        // service = module.get<GenreService>(GenreService);
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                GenreService,
+                {
+                    provide: getRepositoryToken(Genre),
+                    useValue: mockGenreRepository
+                }
+            ]
+        }).compile();
+        genreService = module.get<GenreService>(GenreService);
+        genreRepository = module.get<Repository<Genre>>(getRepositoryToken(Genre));
     });
 
     it('should be defined', () => {
-        // expect(service).toBeDefined();
-        expect(true).toBeDefined();
+        expect(genreService).toBeDefined();
+    });
+
+    describe('findAll', () => {
+        it('should return an array of genres', async () => {
+            const genres = [new Genre(), new Genre()];
+            jest.spyOn(genreRepository, 'find').mockResolvedValue(genres);
+            const result = await genreService.findAll();
+            expect(result).toEqual(genres);
+            expect(genreRepository.find).toHaveBeenCalled();
+        });
+    });
+
+    describe('findOne', () => {
+        it('should return a genre by id', async () => {
+            const genre = new Genre();
+            jest.spyOn(genreRepository, 'findOne').mockResolvedValue(genre as Genre);
+            const result = await genreService.findOne(1);
+            expect(result).toEqual(genre);
+            expect(genreRepository.findOne).toHaveBeenCalledWith({ where: { id: 1 } });
+        });
+    });
+
+    describe('create', () => {
+        it('should create a new genre', async () => {
+            const createGenreDto = { name: 'Action' };
+            jest.spyOn(genreRepository, 'save').mockResolvedValue(createGenreDto as Genre);
+            const result = await genreService.create(createGenreDto as CreateGenreDto);
+            expect(result).toEqual(createGenreDto);
+            expect(genreRepository.save).toHaveBeenCalledWith(createGenreDto);
+        });
+    });
+
+    describe('update', () => {
+        it('should update a genre', async () => {
+            const updateGenreDto = { name: 'Action' };
+            const existingGenre = { id: 1, name: 'Fantasy' };
+            const updatedGenre = { id: 1, name: 'Action' };
+            jest.spyOn(genreRepository, 'findOne').mockResolvedValueOnce(existingGenre as Genre);
+            jest.spyOn(genreRepository, 'findOne').mockResolvedValueOnce(updatedGenre as Genre);
+            const result = await genreService.update(1, updateGenreDto as UpdateGenreDto);
+            expect(result).toEqual(updatedGenre);
+            expect(genreRepository.findOne).toHaveBeenCalledWith({ where: { id: 1 } });
+            expect(genreRepository.update).toHaveBeenCalledWith({ id: 1 }, updateGenreDto);
+        });
+        it('should throw NotFoundException if genre is not found', async () => {
+            const updateGenreDto = { name: 'Action' };
+            jest.spyOn(genreRepository, 'findOne').mockResolvedValue(null);
+            await expect(genreService.update(1, updateGenreDto as UpdateGenreDto)).rejects.toThrow(NotFoundException);
+        });
+    });
+
+    describe('remove', () => {
+        it('should delete a genre', async () => {
+            const existingGenre = { id: 1, name: 'Fantasy' };
+            jest.spyOn(genreRepository, 'findOne').mockResolvedValueOnce(existingGenre as Genre);
+            const result = await genreService.remove(1);
+            expect(result).toEqual(1);
+            expect(genreRepository.delete).toHaveBeenCalledWith({ id: 1 });
+        });
+        it('should throw NotFoundException if genre is not found', async () => {
+            jest.spyOn(genreRepository, 'findOne').mockResolvedValue(null);
+            await expect(genreService.remove(1)).rejects.toThrow(NotFoundException);
+        });
     });
 });
